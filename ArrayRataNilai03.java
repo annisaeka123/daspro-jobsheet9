0b1c5289c50820ddd2c63fc9b73e37a960c70fb3
annisaeka123 <annisaekapuspitaaa@gmail.com>
2023-10-30 05:36:48 +0700

    rata2 nilai

--- ArrayRataNilai03.java
+++ ArrayRataNilai03.java
@@ -6,9 +6,14 @@ import java.util.Scanner;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] nilaiMhs = new int [10];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+
+        int [] nilaiMhs = new int[jmlMhs];
+        double totalLulus = 0, totalTdkLulus = 0;
+        double rataLulus, rataTdkLulus;
+        int luluscount = 0, tdkluluscount = 0;
         double total = 0;
-        double rata2;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -16,9 +21,19 @@ import java.util.Scanner;
         } 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                luluscount++;
+            } else {
+                totalTdkLulus += nilaiMhs[i];
+                tdkluluscount++;
+            } 
+
         }
 
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        rataLulus = totalLulus / luluscount;
+        rataTdkLulus = totalTdkLulus / tdkluluscount;
+        System.out.println("Rata-rata nilai lulus ="+rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus ="+rataTdkLulus);
     }
 }
